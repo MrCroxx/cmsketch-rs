f0fedc518b4127324165890e147722da1db5a8ed
MrCroxx <mrcroxx@outlook.com>
2023-05-18 11:14:21 +0800

    add docs
    Signed-off-by: MrCroxx <mrcroxx@outlook.com>

--- README.md
+++ README.md
@@ -3,3 +3,39 @@
 A count min sketch implementation in Rust.
 
 Ported from [Facebook/CacheLib](https://github.com/facebook/cachelib).
+
+## Usage
+
+```rust
+use cmsketch::CMSketchU32;
+
+const ERROR: f64 = 0.01;
+const PROBABILITY: f64 = 0.95;
+const MAX_WIDTH: usize = 0; // no limits
+const MAX_DEPTH: usize = 0; // no limits
+
+fn main() {
+    let mut cms = CMSketchU32::new(ERROR, PROBABILITY, MAX_WIDTH, MAX_DEPTH);
+    
+    for i in 0..10 {
+        for _ in 0..i {
+            cms.add(i);
+        }
+    }
+    
+    for i in 0..10 {
+        assert!(cms.count(i) >= i as u32);
+    }
+    
+    cms.decay(0.5);
+    
+    for i in 0..10 {
+        assert!(cms.count(i) >= (i as f64 * 0.5) as u32);
+    }
+}
+```
+
+## Roadmap
+
+- [] more key type support
+- [] benchmark
--- src/lib.rs
+++ src/lib.rs
@@ -42,6 +42,36 @@
 //! hash2(1) = 5 -> increment row 2, index 5
 //! hash3(1) = 3 -> increment row 3, index 3
 //! etc.
+//!
+//! # Usage
+//!
+//! ```
+//! use cmsketch::CMSketchU32;
+//!
+//! const ERROR: f64 = 0.01;
+//! const PROBABILITY: f64 = 0.95;
+//! const MAX_WIDTH: usize = 0; // no limits
+//! const MAX_DEPTH: usize = 0; // no limits
+//!
+//! fn main() {
+//!     let mut cms = CMSketchU32::new(ERROR, PROBABILITY, MAX_WIDTH, MAX_DEPTH);
+//!     for i in 0..10 {
+//!         for _ in 0..i {
+//!             cms.add(i);
+//!         }
+//!     }
+//!     
+//!     for i in 0..10 {
+//!         assert!(cms.count(i) >= i as u32);
+//!     }
+//!     
+//!     cms.decay(0.5);
+//!     
+//!     for i in 0..10 {
+//!         assert!(cms.count(i) >= (i as f64 * 0.5) as u32);
+//!     }
+//! }
+//! ```
 
 use paste::paste;
 
